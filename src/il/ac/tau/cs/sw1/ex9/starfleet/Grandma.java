11622858ccc721e47aa19ff2fccf06a85c7db97f
tomerud <rudnitzky@mail.tau.ac.il>
2023-09-10 15:31:52 +0300

    final

--- src/il/ac/tau/cs/sw1/ex9/starfleet/Grandma.java
+++ src/il/ac/tau/cs/sw1/ex9/starfleet/Grandma.java
@@ -1,2 +1,11 @@
-package il.ac.tau.cs.sw1.ex9.starfleet;public class Grandma {
+package il.ac.tau.cs.sw1.ex9.starfleet;
+
+public class Grandma {
+    public int func() {
+        return 1;
+    }
+
+    public int foo() {
+        return 1;
+    }
 }
