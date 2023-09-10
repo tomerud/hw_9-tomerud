c078355690c0a30204c1c7e2f3f8a5aef082b623
tomerud <rudnitzky@mail.tau.ac.il>
2023-09-10 15:40:07 +0300

    Untracked unwanted files

--- src/il/ac/tau/cs/sw1/ex9/starfleet/CrewWoman.java
+++
@@ -1,9 +0,0 @@
-package il.ac.tau.cs.sw1.ex9.starfleet;
-
-public class CrewWoman extends myAbstractCrewMember {
-
-
-	public CrewWoman(int age, int yearsInService, String name){super(age,yearsInService,name);
-
-	}
-}
